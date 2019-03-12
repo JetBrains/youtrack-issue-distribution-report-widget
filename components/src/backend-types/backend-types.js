5fdb2d33efb1fa8266d39b230e880095086a844f
katriyna <katriyna@gmail.com>
2019-03-12 22:30:48 +0300

    updated version of yt with short-types after merging corresponding youtrack branch into develop [publish]

--- components/src/backend-types/backend-types.js
+++ components/src/backend-types/backend-types.js
@@ -1,6 +1,6 @@
 import semver from 'semver';
 
-const YT_VERSIONS_WITH_SHORT_$TYPE = '>=2019.1.50340';
+const YT_VERSIONS_WITH_SHORT_$TYPE = '>=2019.1.50417';
 
 const BackendTypesEnum = {
   SprintBasedBurndownReport: 'jetbrains.youtrack.reports.impl.agile.burndown.gap.SprintBasedBurndownReport',
