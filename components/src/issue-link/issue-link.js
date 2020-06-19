c73b63ce2febccc452f46e7301e4dfffa101bb18
katriyna <katriyna@gmail.com>
2020-06-19 12:03:24 +0300

    JT-57804 show empty values for fields

--- components/src/issue-link/issue-link.js
+++ components/src/issue-link/issue-link.js
@@ -28,10 +28,10 @@ const periodFormatter = value => {
 };
 
 const dateFormatter = value =>
-  fecha.format(value, 'D ddd');
+  fecha.format(value, 'D-MM-YYYY');
 
 const dateAndTimeFormatter = value =>
-  fecha.format(value, 'D ddd HH:mm');
+  fecha.format(value, 'D-MM-YYYY HH:mm');
 
 const floatFormatter = value => value;
 
@@ -57,11 +57,15 @@ const formatAsIssueFieldValue = (value, fieldType) => {
 
 // eslint-disable-next-line complexity
 const getCustomFieldTextPresentation = (field, noEmptyText) => {
-  const value = (field.value && field.value[0]) || field.value;
-  if (value) {
+  const values = Array.isArray(field.value)
+    ? field.value
+    : (field.value && [field.value] || null);
+  if (values) {
     const cfPrototype = (field.projectCustomField || {}).field || {};
     const cfPrototypeType = (cfPrototype.fieldType || {}).valueType;
-    return formatAsIssueFieldValue(value, cfPrototypeType);
+    return values.
+      map(value => formatAsIssueFieldValue(value, cfPrototypeType)).
+      join(', ');
   }
   return noEmptyText
     ? ''
@@ -142,50 +146,52 @@ const IssueLink = (
 
   const tooltip = (
     <div className="yt-issue-preview">
-      <div className="yt-issue-preview__header">
-        {
-          priority &&
-          <span className="yt-issue-preview__priority-wrapper">
-            {priority}
-          </span>
-        }
-        <div>
-          <Link
-            href={`${homeUrl}/issue/${issue.idReadable}`}
-            target="_blank"
-            className={classNames({
-              'yt-issue-preview__id': true,
-              'yt-issue-preview__id_resolved': issue.resolved
-            })}
-          >
-            {issue.idReadable}
-          </Link>
-
-          <span className="yt-issue-preview__summary">
-            {issue.summary}
-          </span>
-        </div>
-      </div>
-
-      <div className="yt-issue-preview__fields">
-        {
-          (!fields || !fields.length) &&
-          <center>
-            <LoaderInline/>
-          </center>
-        }
-
-        {
-          (fieldsValuesToDisplay || []).map(({id, presentation, colorId}) => (
-            <span
-              key={`field-value-${id}`}
-              title={presentation}
-              className={classNames('yt-issue-preview__field', `color-fields__plain-color-${colorId}`)}
+      {
+        priority &&
+        <span className="yt-issue-preview__priority-wrapper">
+          {priority}
+        </span>
+      }
+      <div className="yt-issue-preview__content">
+        <div className="yt-issue-preview__header">
+          <div>
+            <Link
+              href={`${homeUrl}/issue/${issue.idReadable}`}
+              target="_blank"
+              className={classNames({
+                'yt-issue-preview__id': true,
+                'yt-issue-preview__id_resolved': issue.resolved
+              })}
             >
-              {presentation}
+              {issue.idReadable}
+            </Link>
+
+            <span className="yt-issue-preview__summary">
+              {issue.summary}
             </span>
-          ))
-        }
+          </div>
+        </div>
+
+        <div className="yt-issue-preview__fields">
+          {
+            (!fields || !fields.length) &&
+            <center>
+              <LoaderInline/>
+            </center>
+          }
+
+          {
+            (fieldsValuesToDisplay || []).map(({id, presentation, colorId}) => (
+              <span
+                key={`field-value-${id}`}
+                title={presentation}
+                className={classNames('yt-issue-preview__field', `color-fields__plain-color-${colorId}`)}
+              >
+                {presentation}
+              </span>
+            ))
+          }
+        </div>
       </div>
     </div>
   );
